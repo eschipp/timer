fa2ea080e803beaeb1f2095a545f8e3b001dab00
Erin Schippers <52472490+eschipp@users.noreply.github.com>
2022-06-13 22:18:24 -0400

    figuring out how to calc recordedTime

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // define vars to hold time/date values
 let startTime = 0;
 let endTime = 0;
-let today = new Date();
+let now = new Date();
+let recordedTime = 0;
 
 // define var to hold stopwatch status
 let status = "stopped";
@@ -14,26 +15,30 @@ function startStop(){
 	let date = new  Date();
 	if(status === "stopped") {
 		//set start time
-		document.getElementById("startStop").innerHTML = "Stop"
+		document.getElementById("startStop").innerHTML = "Stop";
 		let startTimeObject = document.getElementById("startTimeDisplay").innerHTML = date.toLocaleString([], {
 			hour12: false,
 			hour: '2-digit',
 			minute: '2-digit',
 			second: '2-digit',
 		});
-		let startTime = startTimeObject["hour", "minute", "second"];
-		status = "started"
+		let startTime = new Date().getTime();
+		console.log("this is the start time: ", startTime);
+		status = "started";
 
 	} else {
 		//set end time
-		document.getElementById("startStop").innerHTML = "Start"
+		document.getElementById("startStop").innerHTML = "Start";
 		let endTimeObject = document.getElementById("stopTimeDisplay").innerHTML = date.toLocaleString([], {
 			hour12: false,
 			hour: '2-digit',
 			minute: '2-digit',
 			second: '2-digit',
 		});
-		let endTime = endTimeObject["hour", "minute", "second"];
+		let endTime = new Date().getTime();
+		console.log("this is the end time: ", endTime);
+		let recordedTime = ((endTime - startTime)/60000).toFixed(2);
+		console.log (recordedTime);
 		status = "stopped";
 		
 	}
@@ -42,12 +47,11 @@ function startStop(){
 
 
 
-
 function reset() {
-	let recordedTime = ((endTime - startTime)/60000).toFixed(2);
-	function returnRecordedTime() {
-		return recordedTime;
-	}
+	console.log("start time within reset function: ", startTime)
+	console.log("end time within reset function: ", endTime)
+	
+	
 	if(recordedTime === 0) {
 		
 		document.getElementById("tableTime").innerHTML = "0 minutes"
@@ -55,10 +59,11 @@ function reset() {
 		
 		document.getElementById("tableTime").innerHTML = recordedTime + " minutes";
 	}
-	document.getElementById("tableDate").innerHTML = today.toDateString();
+	document.getElementById("tableDate").innerHTML = now.toDateString();
 	;
 
 	startTime = 0;
 	endTime = 0;
 	status = "stopped";
+
 }

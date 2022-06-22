7561599976fdfb248129894f5c7fccc9a164c1dd
Erin Schippers <52472490+eschipp@users.noreply.github.com>
2022-06-21 22:21:21 -0400

    changing to react

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Goals from './components/Goals';
+import Time from './components/Time';
+import Table from './components/Table';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Goals />
+    <Time />
+    <Table />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
